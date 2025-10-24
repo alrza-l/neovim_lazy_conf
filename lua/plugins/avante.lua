5f5b87321d6474e8cc6a074646634fec5e0b2a76
idlesys <sudo.bahrami.alireza@gmail.com>
2025-10-24 15:23:02 +0330

    test: testing avante AI

---
+++ lua/plugins/avante.lua
@@ -0,0 +1,49 @@
+-- avante.lua
+return {
+  'yetone/avante.nvim',
+  build = 'make',
+  event = 'VeryLazy',
+  version = false,
+  opts = {
+    provider = 'ollama',
+    providers = {
+      ollama = {
+        endpoint = 'http://localhost:8080',
+        model = 'deepseek-coder',
+      },
+    },
+  },
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'MunifTanjim/nui.nvim',
+    'echasnovski/mini.pick',
+    'nvim-telescope/telescope.nvim',
+    'hrsh7th/nvim-cmp',
+    'ibhagwan/fzf-lua',
+    'stevearc/dressing.nvim',
+    'folke/snacks.nvim',
+    'nvim-tree/nvim-web-devicons',
+    'zbirenbaum/copilot.lua',
+    {
+      'HakonHarnes/img-clip.nvim',
+      event = 'VeryLazy',
+      opts = {
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          use_absolute_path = true,
+        },
+      },
+    },
+    {
+      'MeanderingProgrammer/render-markdown.nvim',
+      opts = {
+        file_types = { 'markdown', 'Avante' },
+      },
+      ft = { 'markdown', 'Avante' },
+    },
+  },
+}
